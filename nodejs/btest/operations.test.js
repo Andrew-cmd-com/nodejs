922b9ddf6d0635a89449b1bf438cb0dbd8a464a2
Andrew-cmd-com <78428516+Andrew-cmd-com@users.noreply.github.com>
2023-02-26 17:39:16 +0300

    Delete nodejs directory

--- nodejs/btest/operations.test.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-var operations = require("./operations");
- 
-it("should multiply two numbers", function(){
-     
-    var expected = 15;
-    var result = operations.multiply(3, 5);
-    assert.equal(result, expected);
-});
--- nodejs/des-e/operations.js
+++
@@ -1,2 +0,0 @@
-module.exports.multiply = function(x,y){return x * y;}
-module.exports.add = function(x, y){ return x + y;}
